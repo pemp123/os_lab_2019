c460789c096368dcb52b9959f55c6a3cb792402e
pipl5kmmr <pipl5kmmr@gmail.com>
2020-05-20 16:42:55 +0000

    changed revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,12 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+     char p[128];
+ 
+    strcpy(p,str);
+    int i=0, j=strlen(p)-1;
+    while(i<=j+i)
+     str[i++]=p[j--];
 }
-
